2b2cbcb00878e2eefe7517cb9dbfc68e2784497a
Gliitchhh410 <ahmedmounir532@gmail.com>
2026-01-21 18:30:29 +0200

    fix again: .btn-complete-task & .btn-delete-task overlapping borders

--- script.js
+++ script.js
@@ -402,7 +402,7 @@ class Task {
 
     this.#taskElem.innerHTML = `
       <span class="title-text">${this.title}</span>
-      <button class="btn-delete-task" title="Delete">X</button>
+      <button class="btn-delete-task" title="Delete">x</button>
       <button class="btn-complete-task" title="Complete">✓</button>
       <span class="edit-task-btn" title="Edit">✎</span>
     `
